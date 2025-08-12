00a79ead45c7f17eac80076b2e97ef3078f3c288
Trevinsz <produtoraindi768@gmail.com>
2025-08-12 14:14:29 -0300

    fix: update Vite config and paths for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,21 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
     },
   },
 })
